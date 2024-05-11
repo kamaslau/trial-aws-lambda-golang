f336105d98e6d99f4a97efcf5b2218c9670666b8
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2024-05-12 06:44:42 +0800

    refactor: echo multiple input json field

--- main.go
+++ main.go
@@ -8,14 +8,19 @@ import (
 )
 
 type MyEvent struct {
-	Name string `json:"name"`
+	AppId     string `json:"appId"`
+	AppSecret string `json:"appSecret"`
+	Code      string `json:"code"`
 }
 
 func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
-	message := fmt.Sprintf("Hello %s!", event.Name)
+	message := fmt.Sprintf("AppId: %s\r\n", event.AppId)
+	message += fmt.Sprintf("AppSecret: %s\r\n", event.AppSecret)
+	message += fmt.Sprintf("Code: %s", event.Code)
+	
 	return &message, nil
 }
 
